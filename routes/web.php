ad3383b5558a13fada3b246827831f16acb796ec
mikkiesco <mikkiescorido@gmail.com>
2025-08-01 01:07:06 +0800

    refactor: make json

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Mxent\Pwax\Http\Controllers\PwaxController;
 Route::group(['prefix' => config('pwax.route_prefix', '__pwax__'), 'as' => 'pwax.'], function () {
     Route::get('/{name}.js', [PwaxController::class, 'js'])->name('js');
     Route::get('/{name}.css', [PwaxController::class, 'css'])->name('css');
-    Route::get('/{name}', [PwaxController::class, 'module'])->name('module');
+    Route::get('/{name}.json', [PwaxController::class, 'module'])->name('module');
 });

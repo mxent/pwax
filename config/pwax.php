64f2a97062e1a8e0d88fa0dc93e1a2e47e32705f
mikkiesco <mikkiescorido@gmail.com>
2025-08-02 23:25:03 +0800

    fix: min jquery

--- config/pwax.php
+++ config/pwax.php
@@ -41,6 +41,6 @@ return [
         'https://unpkg.com/vue@3.5.18/dist/vue.global.prod.js',
         'https://unpkg.com/vue-router@4.5.1/dist/vue-router.global.prod.js',
         'https://unpkg.com/pinia@3.0.3/dist/pinia.iife.prod.js',
-        'https://unpkg.com/jquery@3.7.1/dist/jquery.js',
+        'https://unpkg.com/jquery@3.7.1/dist/jquery.min.js',
     ],
 ];

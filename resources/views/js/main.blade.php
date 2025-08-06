17b0daf2efbb67433efaf316c607cdc7926e559e
mikkiesco <mikkiescorido@gmail.com>
2025-08-06 23:08:26 +0800

    fix: re-add pwaximport

--- resources/views/js/main.blade.php
+++ resources/views/js/main.blade.php
@@ -1,5 +1,31 @@
 <script>
 
+    const pwaxHeaders = new Headers();
+    pwaxHeaders.append("Accept", "application/json");
+    pwaxHeaders.append("X-Requested-With", "XMLHttpRequest");
+    pwaxHeaders.append("X-Pwa-Component", "true");
+    window.pwaxHeaders = pwaxHeaders;
+    window.pwaxFetch = async function (url, options = {}) {
+        const f = await fetch(url, options);
+        const j = await f.json();
+        const s = j.script ? await import(data:text/javascript;base64,${btoa(j.script)}) : {};
+        const e = s?.default || {};
+        const v = j.template ? { template: j.template, ...e } : e;
+        return {
+            s: s,
+            v: v
+        };
+    };
+    window.pwaxImports = {};
+    window.pwaxImport = async function (url, name, key = '') {
+        return window.pwaxImports[name] = window.pwaxImports[name] || await (async function() {
+            var r = await window.pwaxFetch(url, {
+                headers: window.pwaxHeaders
+            });
+            return key.length ? r.s[key] : r.v;
+        })();
+    };
+
     document.addEventListener('DOMContentLoaded', async function() {
 
         const baseComp = @import('~pwax/components/vue/app');

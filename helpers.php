f2c9241ae6934297bf31cc3194bb3d96a8206299
mikkiesco <mikkiescorido@gmail.com>
2025-08-10 03:06:26 +0800

    fix: show json when browser back

--- helpers.php
+++ helpers.php
@@ -86,7 +86,11 @@ function vue($blade, $compact = null, $config = []): JsonResponse|RedirectRespon
         return $arr;
     }
 
-    return response()->json($arr);
+    $response = response()->json($arr);
+    $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+    $response->headers->set('Pragma', 'no-cache');
+    $response->headers->set('Expires', 'Thu, 01 Jan 1970 00:00:00 GMT');
+    return $response;
 }
 
 /**

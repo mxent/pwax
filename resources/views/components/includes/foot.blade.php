6810ef4191e2f860d2dd1262fce970e8c91e5cb4
mikkiesco <mikkiescorido@gmail.com>
2025-08-15 23:00:02 +0800

    fix: router for main js

--- resources/views/components/includes/foot.blade.php
+++ resources/views/components/includes/foot.blade.php
@@ -2,7 +2,7 @@
     <script src="{{ $script }}"></script>
 @endforeach
 
-<script src="{{ route('pwax.js', 'pwax__x__js_x_main') }}"></script>
+<script src="{{ router('pwax.js', 'pwax__x__js_x_main') }}"></script>
 
 @if(config('pwax.blade.foot'))
     @include(config('pwax.blade.foot'))
